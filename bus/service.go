50d267d2b7fe9ae83a317ee2594fba42416cd313
likepvc <122666432+likepvc@users.noreply.github.com>
2020-09-20 06:32:24 +0000

    close button

--- bus/service.go
+++ bus/service.go
@@ -5,4 +5,7 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the followin
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistribu
