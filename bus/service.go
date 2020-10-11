6d3dbb5f43942bfd7a661dfc1c015c40d15a4499
likepvc <122666432+likepvc@users.noreply.github.com>
2020-10-11 20:27:20 +0000

    Changed pom.xml

--- bus/service.go
+++ bus/service.go
@@ -13,4 +13,6 @@
  *
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provide
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED 
