bd2178774a8f718dc832a2bc9ac252964cf668e9
likepvc <122666432+likepvc@users.noreply.github.com>
2020-10-14 08:04:02 +0000

    Fixed composer.json

--- bus/service.go
+++ bus/service.go
@@ -15,4 +15,4 @@
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRI
