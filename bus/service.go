bfc2305aea5b641a4290663eebd223bdcc5acd8d
likepvc <122666432+likepvc@users.noreply.github.com>
2021-03-20 06:17:10 +0000

    Initial Commit from Ember CLI v0.2.0-beta.1

--- bus/service.go
+++ bus/service.go
@@ -158,4 +158,7 @@ func (b *Bus) TrackPosition() (float64, float64, *dbus.Error) {
     time, err := b.player.GetTrackPosition(false)
     if err != nil {
         return 0, 0, dbus.NewError("io.github.ghedo.grooved.Error",
-                                   []interface{}{err.Er
+                                   []interface{}{err.Error()})
+    }
+
+    percent, err := b.player.GetTra
