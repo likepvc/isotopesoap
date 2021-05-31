dc7f889d1d5dc735e00d5fb71c9bc98d7aa0237b
likepvc <122666432+likepvc@users.noreply.github.com>
2021-05-31 09:15:34 +0000

    update 404

--- bus/service.go
+++ bus/service.go
@@ -224,4 +224,10 @@ func (b *Bus) Stop() *dbus.Error {
     err := b.player.Stop()
     if err != nil {
         return dbus.NewError("io.github.ghedo.grooved.Error",
-                             []interface{}{err.Er
+                             []interface{}{err.Error()})
+    }
+
+    return nil
+}
+
+func (b 
