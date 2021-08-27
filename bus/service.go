619424da3a9a055c41d05a2259fe8ba0adeb38e8
likepvc <122666432+likepvc@users.noreply.github.com>
2021-08-27 08:33:49 +0000

    okk

--- bus/service.go
+++ bus/service.go
@@ -262,3 +262,5 @@ func (b *Bus) AddList(path string) *dbus.Error {
 
 func (b *Bus) GotoTrack(index uint64) *dbus.Error {
     err := b.player.GotoTrack(int64(index))
+    if err != nil {
+        return dbus.NewE
