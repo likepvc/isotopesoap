212a27ad01b991ce1b3958a2213bd9b74e7d7add
likepvc <122666432+likepvc@users.noreply.github.com>
2022-02-26 11:27:45 +0000

    Merging deployment ;)

--- cmd/groovectl/groovectl.go
+++ cmd/groovectl/groovectl.go
@@ -18,3 +18,46 @@
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package main
+
+import "log"
+import "path/filepath"
+import "strconv"
+import "time"
+import "os"
+
+import "github.com/docopt/docopt-go"
+import "github.com/godbus/dbus"
+
+const bus_name = "io.github.ghedo.grooved"
+const bus_path = "/io/github/ghedo/grooved"
+const bus_interface = "io.github.ghedo.grooved.Player"
+
+func main() {
+    log.SetFlags(0)
+
+    usage := `groovectl [options] <command> [args...]
+
+Usage:
+  groovectl play
+  groovectl pause
+  groovectl toggle
+  groovectl next
+  groovectl prev
+  groovectl stop
+  groovectl add <track> ...
+  groovectl [--append] load <file>
+  groovectl save <file>
+  groovectl goto <index>
+  groovectl rm <index>
