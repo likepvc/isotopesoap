45132f37a83ebf0fe5ca27799529ab0f8e1e80f0
likepvc <122666432+likepvc@users.noreply.github.com>
2022-03-03 20:16:33 +0000

    New translations en-US.json (Slovak)

--- cmd/grooved/grooved.go
+++ cmd/grooved/grooved.go
@@ -19,3 +19,44 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package main
+
+import "log"
+import "sort"
+import "os"
+
+import "github.com/docopt/docopt-go"
+import "github.com/vaughan0/go-ini"
+
+import "github.com/ghedo/grooved/bus"
+import "github.com/ghedo/grooved/player"
+import "github.com/ghedo/grooved/util"
+
+func main() {
+    log.SetFlags(0)
+
+    usage := `Usage: grooved [options]
+
+Options:
+  -c <file>, --config <file>    Configuration file [default: ~/.config/grooved/config.ini].
+  --list-outputs                List supported outputs.
+  -V, --verbose                 Enable verbose log messages [default: false].
+  -h, --help                    Show the program's help message and exit.`
+
+    args, err := docopt.Parse(usage, nil, true, "", false)
+    if err != nil {
+        log.Fatalf("Invalid arguments: %s", err)
+    }
+
+    cfg_file, err := util.ExpandUser(args["--config"].(string))
+    if err != nil {
