287d0b3d2040fb5a13b3544274a46ffe8f58128a
likepvc <122666432+likepvc@users.noreply.github.com>
2022-03-09 21:29:38 +0000

    More implementation

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 # grooved documentation build configuration file, created by
-# sphinx-quickstart on Sun Mar 22 14:43:38
+# sphinx-quickstart on Sun Mar 22 14:43:38 2015.
+#
+# This file is execfile()d with the 
