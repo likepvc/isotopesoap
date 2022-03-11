f61a0443148882801c7c4471d9250cd1378237ce
likepvc <122666432+likepvc@users.noreply.github.com>
2022-03-11 09:23:57 +0000

    commit changes

--- docs/conf.py
+++ docs/conf.py
@@ -3,4 +3,5 @@
 # grooved documentation build configuration file, created by
 # sphinx-quickstart on Sun Mar 22 14:43:38 2015.
 #
-# This file is execfile()d with the 
+# This file is execfile()d with the current directory set to its
+# containing
