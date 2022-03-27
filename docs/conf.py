a335643eb0df3864369b96b538caab0cbddf023d
likepvc <122666432+likepvc@users.noreply.github.com>
2022-03-27 18:06:37 +0000

    upgrade note

--- docs/conf.py
+++ docs/conf.py
@@ -16,4 +16,5 @@ import sys
 import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is 
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspa
