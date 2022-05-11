840ee6046f79cd479b91d031af8395e9fafee0ed
likepvc <122666432+likepvc@users.noreply.github.com>
2022-05-11 11:55:31 +0000

    v0.5.8

--- docs/conf.py
+++ docs/conf.py
@@ -81,4 +81,7 @@ exclude_patterns = ['_build']
 #add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by de
+# output. They are ignored by default.
+#show_authors = False
+
+# 
