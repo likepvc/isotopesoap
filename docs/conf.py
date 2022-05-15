cf0fa987127a0a015dc44505c7b7156906cc5e3d
likepvc <122666432+likepvc@users.noreply.github.com>
2022-05-15 20:37:04 +0000

    Added badges.

--- docs/conf.py
+++ docs/conf.py
@@ -87,4 +87,5 @@ exclude_patterns = ['_build']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# A list of ignored prefixes fo
+# A list of ignored prefixes for module index sorting.
+#modindex_common_prefix
