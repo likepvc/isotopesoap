dae098376a5660c9b2594134681d99ec47abdec4
likepvc <122666432+likepvc@users.noreply.github.com>
2022-05-23 05:36:17 +0000

    Small code cleanups

--- docs/conf.py
+++ docs/conf.py
@@ -90,4 +90,8 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
-# If true, keep warnings as "system message" paragraphs in the built documents
+# If true, keep warnings as "system message" paragraphs in the built documents.
+#keep_warnings = False
+
+
+# -- Options f
