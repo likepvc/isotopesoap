1182bf6ceba301ab9578178140f526e01ec3f2d2
likepvc <122666432+likepvc@users.noreply.github.com>
2022-05-25 22:50:43 +0000

    ASoC: wm_adsp: Avoid attempt to free buffers that might still be in use

--- docs/conf.py
+++ docs/conf.py
@@ -96,4 +96,6 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the docu
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+ht
