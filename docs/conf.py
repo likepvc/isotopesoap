93e0f521d62f32d6123d29f70e97b775c38306f6
likepvc <122666432+likepvc@users.noreply.github.com>
2022-05-30 13:41:36 +0000

    merging refs/remotes/origin/master into HEAD

--- docs/conf.py
+++ docs/conf.py
@@ -102,4 +102,7 @@ html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
-#
+# documentation.
+#html_theme_options = {}
+
+# Add any 
