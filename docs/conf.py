71f5448ad9a710b57ff523b156078dbf057a0643
likepvc <122666432+likepvc@users.noreply.github.com>
2022-06-20 18:34:39 +0000

    Added setup files

--- docs/conf.py
+++ docs/conf.py
@@ -126,4 +126,4 @@ html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-#
+# so a file named "default.css" will overwrite the 
