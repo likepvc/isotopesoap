698b4905849dc938c11957bbfa58a9987f92355f
likepvc <122666432+likepvc@users.noreply.github.com>
2022-06-21 16:08:21 +0000

    correct link

--- docs/conf.py
+++ docs/conf.py
@@ -126,4 +126,5 @@ html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the 
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path =
