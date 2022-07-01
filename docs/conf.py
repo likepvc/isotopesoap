06b17a75c93e7f4d1b4e0113f2d8503062a68002
likepvc <122666432+likepvc@users.noreply.github.com>
2022-07-01 08:04:33 +0000

    Fix Qt build.

--- docs/conf.py
+++ docs/conf.py
@@ -134,4 +134,5 @@ html_static_path = ['_static']
 # directly to the root of the documentation.
 #html_extra_path = []
 
-# If not '', a 'Last updated on:' timestamp is ins
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given
