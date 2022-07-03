0dfe1db48bed15dc8163a200e75b1467b7efdfb4
likepvc <122666432+likepvc@users.noreply.github.com>
2022-07-03 15:41:34 +0000

    nothing change

--- docs/conf.py
+++ docs/conf.py
@@ -135,4 +135,5 @@ html_static_path = ['_static']
 #html_extra_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given
+# using the given strftime format.
+#html_last_updated_fmt =
