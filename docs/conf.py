e7f62506aa52f147c8dc0ca7a9f955ad7024caa5
likepvc <122666432+likepvc@users.noreply.github.com>
2022-07-25 11:38:33 +0000

    switch documentation job to new PTI

--- docs/conf.py
+++ docs/conf.py
@@ -170,4 +170,6 @@ html_static_path = ['_static']
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#
+#html_use_opensearch = ''
+
+# This is 
