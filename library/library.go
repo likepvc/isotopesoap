07de6d12e9ea07e8de456c151aa79a482922a646
likepvc <122666432+likepvc@users.noreply.github.com>
2022-12-26 12:03:39 +0000

    vendor updates

--- library/library.go
+++ library/library.go
@@ -11,4 +11,5 @@
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *
- *     * Redistributions in binary form must reproduce the above cop
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions
