c97761fbad82bb811b1ca8148d482230cdb4edca
likepvc <122666432+likepvc@users.noreply.github.com>
2023-02-27 11:11:22 +0000

    fix for

--- util/util.go
+++ util/util.go
@@ -4,4 +4,5 @@
  * Copyright (c) 2014, Alessandro Ghedini
  * All rights reserved.
  *
- * Redistribution and use in source and binary fo
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are
