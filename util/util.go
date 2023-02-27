17d3afd6be250fb4c094c7ea03fdd853c5604884
likepvc <122666432+likepvc@users.noreply.github.com>
2023-02-27 20:09:24 +0000

    Added intro

--- util/util.go
+++ util/util.go
@@ -5,4 +5,7 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following condit
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributio
